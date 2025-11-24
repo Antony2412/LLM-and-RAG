5bb1d673374cb43dfc6a51f527c9cca7c4c0f7f7
Antony Joy <89941221+Antony2412@users.noreply.github.com>
2025-11-24 16:51:09 +0530

    Prompt Template LLM
    Did  the invockation of llm by using Prompt template and used the promt to get the result .

--- langchain.py
+++ langchain.py
@@ -2,6 +2,7 @@ import os
 import streamlit as st
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import SystemMessage,HumanMessage
+from langchain_core.prompts import ChatPromptTemplate
 from constants import openai_key
 from dotenv import load_dotenv
 
@@ -13,9 +14,18 @@ llm = ChatOpenAI()
 #response = llm.invoke("Suggest me a title of movie in malayalam to watch ")
 #print(response.content)
 
-messages=[
-    SystemMessage("Translate the following from English to hindi"),
-    HumanMessage("Hi nice to meet you")
-]
-response=llm.invoke(messages)
+#messages=[
+#    SystemMessage("Translate the following from English to hindi"),
+#    HumanMessage("Hi nice to meet you")
+#]
+
+system_template = "Translate the follong form English to {language}"
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system",system_template),"user","{text}"]
+)
+
+prompt = prompt_template.invoke({"language":"Malayalam", "text":"Hi nice to meet you"})
+
+response=llm.invoke(prompt)
 print(response.content)
